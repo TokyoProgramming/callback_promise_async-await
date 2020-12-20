a4e336d5720d0c666a87972cf8637275c872bf85
TokyoProgramming <samuraidjango.jp@gmail.com>
2020-12-21 00:23:41 +0900

    commit four

--- 3/fetch.js
+++ 3/fetch.js
@@ -1,3 +1,11 @@
+const textId = document
+  .getElementById('text')
+  .addEventListener('click', showText);
+
+const jsonId = document
+  .getElementById('json')
+  .addEventListener('click', showJson);
+
 fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => response.json())
   .then((json) => console.log(json))
@@ -23,23 +31,41 @@ async function getJson() {
 
   return data;
 }
-getJson().then((users) => {
-  let output = '';
-  users.forEach((user) => {
-    output += `
-	<div>${user.id}</div>
-	<li>${user.name}</li>
-	<li>${user.username}</li>
+// getJson().then((users) => {
+//   let output = '';
+//   users.forEach((user) => {
+//     output += `
+// 	<div>${user.id}</div>
+// 	<li>${user.name}</li>
+// 	<li>${user.username}</li>
 
-	`;
-  });
-  document.body.innerHTML = output;
-});
+// 	`;
+//   });
+//   document.body.innerHTML = output;
+// });
 
 async function getText() {
   const text = await fetch('simple.txt');
   const re = text.text();
+
   return re;
 }
+function showText() {
+  getText().then((data) => console.log(data));
+}
 
-getText().then((data) => console.log(data));
+function showJson() {
+  getJson().then((data) => console.log(data));
+  getJson().then((users) => {
+    let output = '';
+    users.forEach((user) => {
+      output += `
+	  <div>${user.id}</div>
+	  <li>${user.name}</li>
+	  <li>${user.username}</li>
+  
+	  `;
+    });
+    document.body.innerHTML = output;
+  });
+}
